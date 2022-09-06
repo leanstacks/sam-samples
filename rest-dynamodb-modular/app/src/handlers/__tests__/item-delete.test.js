b573e5a3ca5a10a10036e3dfe3bf9d3f24e2fbe2
Matthew Warman <warmanmr@gmail.com>
2022-09-06 14:40:41 -0400

    delete item handler tests

--- rest-dynamodb-modular/app/src/handlers/__tests__/item-delete.test.js
+++ rest-dynamodb-modular/app/src/handlers/__tests__/item-delete.test.js
@@ -1,40 +1,28 @@
-// Import all functions from delete-item.js
+// import module to be tested
 const lambda = require('../item-delete');
-// Import dynamodb from aws-sdk
-const { DynamoDBDocumentClient } = require('@aws-sdk/lib-dynamodb');
+
+// import modules to be mocked
+const itemService = require('../../services/item-service');
+jest.mock('../../services/item-service');
+
+// import test fixtures
+const itemFixtures = require('../../__fixtures__/items');
 
 // This includes all tests for FindItem handler
-describe('handler::DeleteItem', () => {
-  let sendSpy;
-
-  // Test one-time setup and teardown, see more in https://jestjs.io/docs/en/setup-teardown
-  beforeAll(() => {
-    // Mock dynamodb
-    // https://jestjs.io/docs/jest-object#jestspyonobject-methodname
-    sendSpy = jest.spyOn(DynamoDBDocumentClient.prototype, 'send');
-  });
-
-  // Reset mocks to their original state
+describe('DeleteItem::handle', () => {
   afterEach(() => {
-    sendSpy.mockReset();
-  });
-
-  // Clean up mocks
-  afterAll(() => {
-    sendSpy.mockRestore();
+    itemService.delete.mockClear();
   });
 
   it('should return status code 204 when successful', async () => {
-    const item = { id: 'id1' };
-
-    // Return the specified value whenever the spied function is called
-    sendSpy.mockResolvedValue({});
+    itemService.delete.mockResolvedValueOnce(null);
 
     const event = {
       httpMethod: 'DELETE',
       pathParameters: {
-        itemId: 'id1',
+        itemId: 'a1',
       },
+      body: null,
     };
 
     // Invoke the handler
@@ -44,8 +32,33 @@ describe('handler::DeleteItem', () => {
       statusCode: 204,
     };
 
-    // Expect dynamodb to have been called
-    expect(sendSpy).toHaveBeenCalledTimes(1);
+    // Expect the service to have been called
+    expect(itemService.delete).toHaveBeenCalledTimes(1);
+    // Compare the result with the expected result
+    expect(result).toEqual(expectedResult);
+  });
+
+  it('should return statusCode 500 when an error occurs', async () => {
+    itemService.delete.mockRejectedValueOnce(new Error('test'));
+
+    const event = {
+      httpMethod: 'DELETE',
+      pathParameters: {
+        itemId: 'a1',
+      },
+      body: null,
+    };
+
+    // Invoke the handler
+    const result = await lambda.handle(event);
+
+    const expectedResult = {
+      statusCode: 500,
+      body: JSON.stringify({ name: 'Error', message: 'test' }),
+    };
+
+    // Expect the service to have been called
+    expect(itemService.delete).toHaveBeenCalledTimes(1);
     // Compare the result with the expected result
     expect(result).toEqual(expectedResult);
   });
